e4e6c7ec93e363836fe9e33a68979012e1b5c3c1
Talaini <talainimohammed@gmail.com>
2023-04-24 16:33:57 +0100

    updates

--- _putchar.c
+++ _putchar.c
@@ -10,5 +10,18 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	char buf[1024];
+	int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
--- main.h
+++ main.h
@@ -6,19 +6,18 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int printf_p(va_list val);
-int printf_task5(va_list val);
-int printf_X(va_list val);
-int printf_x(va_list val);
-int printf_o(va_list val);
-int printf_u(va_list args);
+int printf_p();
+int printf_task5();
+int printf_X();
+int printf_x();
+int printf_o();
+int printf_u();
 int printf_b(unsigned int num);
-int printf_r(va_list args);
-int printf_rot13(va_list args);
-int printf_i(va_list args);
-int printf_d(va_list args);
+int printf_r();
+int printf_rot13();
+int printf_i();
+int printf_d();
 int printf_percent(void);
-int printf_c(char c);
 int printf_str(char *str);
 int _strlen(char *s);
 #endif
