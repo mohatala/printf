0fb72e08dfd8c836f37b0d8ac0e860747e4f2b65
Talaini <talainimohammed@gmail.com>
2023-04-24 16:59:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,8 +9,12 @@
  */
 int main(void)
 {
+    void *addr;
+
+    addr = (void *)0x7ffe637541f0;
     _printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     _printf("%b\n", 98);
+    _printf("Address:[%p]\n", addr);
     return (0);
 }
